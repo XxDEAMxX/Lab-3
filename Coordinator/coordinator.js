87b94acacada6ec2d5efb283c4cba5bc78657758
Daniel <daniel.arevalo@uptc.edu.co>
2024-11-06 23:55:48 -0500

    feat: finish local

--- Coordinator/coordinator.js
+++ Coordinator/coordinator.js
@@ -31,6 +31,8 @@ app.use((req, res, next) => {
 wss.on('connection', (ws) => {
   connectedClients.push(ws);
 
+  ws.send(JSON.stringify([...instances]));
+
   ws.on('close', () => {
     connectedClients = connectedClients.filter(client => client !== ws);
   });
@@ -61,58 +63,58 @@ function getRandomPort(min, max, usedPorts) {
 
 function registerInstance(host, port) {
   instances.push({ host, port });
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify({  host, port }));
+    }
+});
   logMessage(`Instancia registrada: ${host}:${port}`);
 }
 
 app.post('/register', (req, res) => {
-  console.log('si llego');
-  // console.log(req.body);
   const { host, port } = req.body;
   registerInstance(host, port);
-  console.log(host, port);
-  console.log(port);
-  console.log(host);
+  console.log(instances);
   res.status(200).send('Instancia registrada');
 });
 
 app.post('/launch', (req, res) => {
-  console.log("Lanzando instancia");
-  // const connection = new Client();
-  // connection.on('ready', () => {
-  //   logMessage("Cliente SSH conectado");
+  const connection = new Client();
+  connection.on('ready', () => {
+    logMessage("Cliente SSH conectado");
 
-  //   const randomPort = getRandomPort(5000, 6000, usedPorts);
-  //   const dockerCommand = `docker run -d -p ${randomPort}:3000 instance`;
+    const randomPort = getRandomPort(5000, 6000, usedPorts);
+    const dockerCommand = `docker run -d -p ${randomPort}:3000 instance`;
 
-  //   logMessage(`Ejecutando comando Docker: ${dockerCommand}`);
+    logMessage(`Ejecutando comando Docker: ${dockerCommand}`);
 
-  //   connection.exec(dockerCommand, (err, stream) => {
-  //     if (err) {
-  //       logMessage(`Error ejecutando el comando Docker: ${err.message}`);
-  //       connection.end();
-  //       return res.status(500).send('Error ejecutando el comando Docker');
-  //     }
+    connection.exec(dockerCommand, (err, stream) => {
+      if (err) {
+        logMessage(`Error ejecutando el comando Docker: ${err.message}`);
+        connection.end();
+        return res.status(500).send('Error ejecutando el comando Docker');
+      }
 
-  //     stream.on('close', async (code, signal) => {
-  //       registerInstance(host, randomPort);
-  //       usedPorts.push(randomPort);
-  //       connection.end();
-  //       res.status(200).send(`Instancia lanzada en puerto: ${randomPort}`);
-  //     }).on('data', (data) => {
-  //       console.log(`STDOUT: ${data}`);
-  //     }).stderr.on('data', (data) => {
-  //       console.error(`STDERR: ${data}`);
-  //     });
-  //   });
-  // }).on('error', (err) => {
-  //   logMessage(`Error de conexión: ${err.message}`);
-  //   res.status(500).send('Error de conexión SSH');
-  // }).connect({
-  //   host: host,
-  //   port: 22,
-  //   username: username,
-  //   password: password,
-  // });
+      stream.on('close', async (code, signal) => {
+        registerInstance(host, randomPort);
+        usedPorts.push(randomPort);
+        connection.end();
+        res.status(200).send(`Instancia lanzada en puerto: ${randomPort}`);
+      }).on('data', (data) => {
+        console.log(`STDOUT: ${data}`);
+      }).stderr.on('data', (data) => {
+        console.error(`STDERR: ${data}`);
+      });
+    });
+  }).on('error', (err) => {
+    logMessage(`Error de conexión: ${err.message}`);
+    res.status(500).send('Error de conexión SSH');
+  }).connect({
+    host: host,
+    port: 22,
+    username: username,
+    password: password,
+  });
 });
 
 app.get('/instances', (req, res) => {
@@ -139,8 +141,7 @@ async function getInstanceTimes() {
 }
 
 async function synchronizeClocks(req, res) {
-  const coordinatorTime = getWorldTime();
-  //const coordinatorTime = new Date();
+  const coordinatorTime = await getWorldTime();
   const instanceTimes = await getInstanceTimes();
 
   const offsets = instanceTimes.map(({ host, port, time }) => {
@@ -149,11 +150,13 @@ async function synchronizeClocks(req, res) {
   });
 
   const averageOffset = offsets.reduce((sum, { offset }) => sum + offset, 0) / offsets.length;
-
   for (const { host, port, offset } of offsets) {
+    console.log('offset:' + offset);
+    console.log('averageOffset:' + averageOffset);
     const correction = averageOffset - offset;
+    console.log('correction:' + correction);
     try {
-      await axios.post(`http://${host}:${port}/sync`, { offset: correction });
+      await axios.post(`http://${host}:${port}/sync`, { body: averageOffset });
       logMessage(`Sincronizado ${host}:${port} con desfase: ${correction} segundos`);
     } catch (error) {
       logMessage(`Error al sincronizar la instancia ${host}:${port}: ${error.message}`);
@@ -163,12 +166,19 @@ async function synchronizeClocks(req, res) {
   res.status(200).json({ message: 'Sincronización completada' });
 }
 
+async function getWorldTime() {
+  try {
+    const response = await axios.get('https://timeapi.io/api/time/current/zone?timeZone=America%2FBogota');
+    return new Date(response.data.dateTime);
+  } catch (error) {
+    logMessage(`Error fetching world time: ${error.message}`);
+    throw new Error('Error fetching world time');
+  }
+}
+
 app.post('/sync-clocks', synchronizeClocks);
 
 const server = app.listen(port, () => {
-  console.log(host, password, username);
-  console.log(username);
-
   logMessage(`Coordinator running at ${port}`);
 });
 
@@ -181,16 +191,6 @@ const server = app.listen(port, () => {
       res.status(500).send('Error fetching world time');
     });
 });*/
-
-async function getWorldTime() {
-  try {
-    const response = await axios.get('http://worldtimeapi.org/api/timezone/America/Bogota');
-    return response.data.datetime;
-  } catch (error) {
-    logMessage(`Error fetching world time: ${error.message}`);
-    throw new Error('Error fetching world time');
-  }
-}
 
 app.get('/worldtime', async (req, res) => {
   try {
--- Instance/instance.js
+++ Instance/instance.js
@@ -3,12 +3,16 @@ const dotenv = require('dotenv');
 const WebSocket = require('ws');
 
 const app = express();
-const port = process.env.INSTANCE_PORT || 3000;
-dotenv.config();
+dotenv.config( { path: './.env' } );
+app.use(express.json()); 
 
 let logicalTime = new Date();
 let logs = []; 
 const clients = new Set(); 
+
+const port = process.env.PORT || 3000;
+const hostIp = process.env.HOST_IP || '192.168.1.14';
+const localIp = process.env.LOCAL_IP || '192.168.1.14';
 
 function getFormattedTime() {
     const hours = String(logicalTime.getHours()).padStart(2, '0');
@@ -59,11 +63,12 @@ app.get('/time', (req, res) => {
 });
 
 app.post('/sync', express.json(), (req, res) => {
-    const { offset } = req.body; 
+    const { body } = req.body; 
+    console.log(body);
 
-    if (typeof offset === 'number') {
-        logicalTime.setSeconds(logicalTime.getSeconds() + offset);
-        logMessage(`Synchronized logical time with offset: ${offset} seconds`);
+    if (typeof body === 'number') {
+        logicalTime.setSeconds(logicalTime.getSeconds() + body);
+        logMessage(`Synchronized logical time with offset: ${body} seconds`);
         res.status(200).json({ message: 'Logical time synchronized successfully', newTime: getFormattedTime() });
     } else {
         res.status(400).json({ error: 'Invalid offset value. It must be a number.' });
@@ -71,8 +76,9 @@ app.post('/sync', express.json(), (req, res) => {
 });
 
 async function register()  {
+
     try {
-        const response = await fetch('http://localhost:4000/register', 
+        const response = await fetch(`http://localhost:4000/register`, 
             { 
                 method: 'POST', 
                 headers: {
@@ -106,7 +112,7 @@ wss.on('connection', (ws) => {
     logMessage('New client connected');
     clients.add(ws);
 
-    ws.send(JSON.stringify({ time: getFormattedTime() }));
+    ws.send(JSON.stringify({ time: 'getFormattedTime()' }));
 
     ws.on('close', () => {
         logMessage('Client disconnected');
